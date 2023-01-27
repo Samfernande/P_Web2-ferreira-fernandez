3f13ca78f1ac773ecaf71225edd28f5052b928e1
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-01-27 11:09:21 +0100

    sql

--- website/index.php
+++ website/index.php
@@ -6,8 +6,13 @@
         include 'controller/ControllerBasePage.php';
         WriteBasePage(0);
 
-        // Se connecter via PDO
-        $connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
+        
+
+    // Se connecter via PDO
+    $connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
+
+    $req = $connector->query("INSERT INTO db_cif.t_categorie (catTitre) VALUES ('Livre')");
+
     ?>
 
 <html>
