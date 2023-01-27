01873acef32294fe720c604190a40da50cf5edf1
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-01-27 11:24:04 +0100

    test

--- website/index.php
+++ website/index.php
@@ -11,8 +11,17 @@
     // Se connecter via PDO
     $connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
 
-    $req = $connector->query("INSERT INTO db_cif.t_categorie (catTitre) VALUES ('Livre')");
+    /*
+    $catArray = array("Livre", "Sport", "Art", "Nature", "Voyage", "Culture", "Photographie", "Gastronomie", "Science", "Humanitaire", "Technologie", "Cinéma", "Histoire", "Musique", "Science-fiction", "Fantasy", "Mode");
+    $idArray = range(1, count($catArray));
 
-    ?>
+    $req = $connector->query("DELETE FROM db_cif.t_categorie");
 
+    for ($i = 0; $i < count($catArray); $i++) {
+        $query = "INSERT INTO t_categorie (idCategorie, catTitre) VALUES ($idArray[$i], '$catArray[$i]')";
+        $connector->query($query);
+    }
+    */
+
+?>
 <html>
