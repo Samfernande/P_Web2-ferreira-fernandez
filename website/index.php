d31cfc77f47026db63b787d07059700c6d77fe5e
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-01-30 11:18:41 +0100

    cif

--- website/index.php
+++ website/index.php
@@ -4,18 +4,10 @@
 
     <?php 
     include_once "controller/ControllerMain.php";
-    include_once "model/ModelUser.php";
+    include_once "model/ModelCif.php";
 
         $controllerMain = new ControllerMain();
-        $modelUser = new ModelUser();
-
-        for ($i=0; $i < 6 ; $i++){
-
-            $modelUser->addUser("Michel" . $i, "Michel" . $i);
-            
-        }
-
-        
+        $modelCif = new ModelCif();
         
     ?>
 
