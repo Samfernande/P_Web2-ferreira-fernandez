ac980e099c91c38e78ee3475fa100c92c3a0d753
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-01-30 10:49:23 +0100

    user

--- website/model/ModelCategorie.php
+++ website/model/ModelCategorie.php
@@ -4,7 +4,7 @@ include_once "model.php";
 
 class ModelCategorie extends Model{
 
-    public function addData(){
+    private function addData(){
 
         $catArray = array("Livre", "Sport", "Art", "Nature", "Voyage", "Culture", "Photographie", "Gastronomie", "Science", "Humanitaire", "Technologie", "Cinéma", "Histoire", "Musique", "Science-fiction", "Fantasy", "Mode");
         $idArray = range(1, count($catArray));
@@ -13,8 +13,8 @@ class ModelCategorie extends Model{
         $cat = array_combine($idArray, $catArray);
 
         foreach ($cat as $id => $categorie) {
-            $checkStmt = $this->connector->prepare("SELECT * FROM db_cif.t_categorie WHERE catTitre = :catTitre");
-            $checkStmt->bindParam(':catTitre', $categorie);
+            $checkStmt = $this->connector->prepare("SELECT * FROM db_cif.t_categorie WHERE idCategorie = :idCategorie");
+            $checkStmt->bindParam(':idCategorie', $id);
             $checkStmt->execute();
           
             if ($checkStmt->rowCount() == 0) {
--- website/model/ModelUser.php
+++ website/model/ModelUser.php
@@ -3,5 +3,14 @@
 include_once "model.php";
 
 class ModelUser extends Model{
+    public function addUser($userPseudo, $userPassword){
+        $date = date('y-m-d');
 
+        $stmt = $this->connector->prepare("INSERT INTO db_cif.t_utilisateur (utiPseudo, utiMotDePasse, utiDateEntree) VALUES (:utiPseudo, :utiMotDePasse, :utiDateEntree)");
+
+        $stmt->bindParam(':utiPseudo', $userPseudo);
+        $stmt->bindParam(':utiMotDePasse', $userPassword);
+        $stmt->bindParam(':utiDateEntree', $date);
+        $stmt->execute();
+    } 
 }
