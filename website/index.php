934cf1a5979688483e9ed5798e758738061af942
Samuel Fernandez <113102356+Samfernande@users.noreply.github.com>
2023-01-30 08:55:34 +0100

    Model

--- website/index.php
+++ website/index.php
@@ -4,34 +4,12 @@
 
     <?php 
         include_once 'view/view.php';
+        include_once 'model/model.php';
 
+        $model = new Model();
         $view = new View();
 
         $view->render('main.php');
-
-
-    $connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
-
-
-
-    $catArray = array("Livre", "Sport", "Art", "Nature", "Voyage", "Culture", "Photographie", "Gastronomie", "Science", "Humanitaire", "Technologie", "Cinéma", "Histoire", "Musique", "Science-fiction", "Fantasy", "Mode");
-    $idArray = range(1, count($catArray));
-
-    $cat = array_combine($idArray, $catArray);
-
-    
-    $req = $connector->query('DELETE FROM db_cif.t_categorie');
- 
-    foreach ($cat as $id => $categorie) {
-        $stmt = $connector->prepare("INSERT INTO db_cif.t_categorie (idCategorie, catTitre)
-        VALUES (:idCategorie, :catTitre)");
-        $stmt->bindParam(':idCategorie', $id);
-        $stmt->bindParam(':catTitre', $categorie);
-        $stmt->execute();
-      }
-
-
-    
     ?>
 
 <html>
--- website/model/model.php
+++ website/model/model.php
@@ -1,4 +1,33 @@
 <?php
+
+// Classe modèle permettant d'intéragir avec la base de données
+
 class Model {
+
+    public $connector;
+
+    public function __construct() {
+
+        $this->connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
+    }
+
+    public function addData($idArray, $catArray){
+
+            //$catArray = array("Livre", "Sport", "Art", "Nature", "Voyage", "Culture", "Photographie", "Gastronomie", "Science", "Humanitaire", "Technologie", "Cinéma", "Histoire", "Musique", "Science-fiction", "Fantasy", "Mode");
+            //$idArray = range(1, count($catArray));
+
+            $cat = array_combine($idArray, $catArray);
+
+    
+            $req = $this->connector->query('DELETE FROM db_cif.t_categorie');
+ 
+            foreach ($cat as $id => $categorie) {
+                $stmt = $this->connector->prepare("INSERT INTO db_cif.t_categorie (idCategorie, catTitre)
+                VALUES (:idCategorie, :catTitre)");
+                $stmt->bindParam(':idCategorie', $id);
+                $stmt->bindParam(':catTitre', $categorie);
+                $stmt->execute();
+            }
+    }
     
 }
