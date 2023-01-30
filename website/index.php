5ba776af9dfc7a26dda6cfac1c658bb853c58aa9
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-01-30 08:45:15 +0100

    inserer_donnes

--- website/index.php
+++ website/index.php
@@ -9,22 +9,29 @@
 
         $view->render('main.php');
 
-    // Se connecter via PDO
-    //$connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
+
+    $connector = new PDO('mysql:host=localhost:6044;dbname=mydb;charset=utf8' , 'root', 'root');
 
 
-    /* TRUCS DE JOAO
 
     $catArray = array("Livre", "Sport", "Art", "Nature", "Voyage", "Culture", "Photographie", "Gastronomie", "Science", "Humanitaire", "Technologie", "Cinéma", "Histoire", "Musique", "Science-fiction", "Fantasy", "Mode");
     $idArray = range(1, count($catArray));
 
-    $req = $connector->query("DELETE FROM db_cif.t_categorie");
+    $cat = array_combine($idArray, $catArray);
 
-    for ($i = 0; $i < count($catArray); $i++) {
-        $query = "INSERT INTO t_categorie (idCategorie, catTitre) VALUES ($idArray[$i], '$catArray[$i]')";
-        $connector->query($query);
-    }
-    */
+    
+    var_dump($cat);
+ 
+    foreach ($cat as $id => $categorie) {
+        $stmt = $connector->prepare("INSERT INTO db_cif.t_categorie (idCategorie, catTitre)
+        VALUES (:idCategorie, :catTitre)");
+        $stmt->bindParam(':idCategorie', $id);
+        $stmt->bindParam(':catTitre', $categorie);
+        $stmt->execute();
+      }
+
+
+    
     ?>
 
 <html>
