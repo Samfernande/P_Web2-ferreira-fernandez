b77722fcfa8e539e21ed0fdf1e8440e826817fe8
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-02-09 15:12:05 +0100

    description

--- website/view/page/main.php
+++ website/view/page/main.php
@@ -28,7 +28,7 @@
     <div class = 'horizontalLine'></div>
     <p class = "colorDarkBlue noMarge">CIF.com est un site web/application qui permet aux utilisateurs de partager et découvrir les coups de coeur dans diverses catégories telles que les livres, les albums, les jeux vidéo, les restaurants, les activités, etc. CIF signifie "Coup de Coeur" en français.</p>
     <br>
-    <p class = "colorDarkBlue noMarge">Notre site vous permet de visualiser la liste complète des CIF stockées dans notre base de données, ainsi que les évaluations associées. Vous pouvez également partager vos propres coups de coeur en remplissant simplement un formulaire sur notre page "Ajout d'une CIF".</p>
+    <p class = "colorDarkBlue noMarge">Notre site vous permet de visualiser la liste complète des CIF stockées dans notre base de données, ainsi que les évaluations associées. Vous pouvez également partager vos propres coups de coeur en remplissant simplement un formulaire sur notre page "Ajouter une CIF".</p>
     <br>
     <p class = "colorDarkBlue noMarge">Notre objectif est de permettre aux utilisateurs de découvrir de nouveaux coups de coeur dans leurs centres d'intérêt et de partager leurs propres recommandations avec la communauté. Rejoignez-nous dès maintenant pour découvrir de nouveaux coups de coeur et partager les vôtres !</p>
     <br>
