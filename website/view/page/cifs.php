22fbdd50920bcbbbc19571a9cce8837ab44ca849
Samuel Fernandez <113102356+Samfernande@users.noreply.github.com>
2023-02-23 11:03:27 +0100

    allCif

--- website/view/page/cifs.php
+++ website/view/page/cifs.php
@@ -1,15 +1,154 @@
-<div class = "backgroundBlue littlePadding">
-<br><br>
-    <div class = "containerLeft">
-        <?php ?>
+<div class = 'backgroundLightBlue littlePadding'>
+
+    <div class ='card backgroundDarkBlue'>
+
+        <div class='containerSpaceBetween '>
+            <h1 class='colorWhite'>J'adore beaucoup les saucisses, mais genre vraiment</h1>
+            <p class='colorWhite'>Restaurant</p>
+            
+
+        </div>
+
+        <div class='containerRightColumns littleSideMarge'>
+
+        <p class='colorWhite'>IMAGE</p>
+
+        </div>
+        
+        <div class='containerSpaceBetween '>
+            <p class='colorWhite textSmall'>Jean Michel</p>
+            <div class='containerMiddle'>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+
+            </div>
+        </div>
+        
 
     </div>
 
-</div>
+    <div class ='card backgroundDarkBlue'>
 
-<div class = 'backgroundLightBlue littlePadding'>
+        <div class='containerSpaceBetween '>
+            <h1 class='colorWhite'>J'adore beaucoup les saucisses, mais genre vraiment</h1>
+            <p class='colorWhite'>Restaurant</p>
+            
 
+        </div>
 
+        <div class='containerRightColumns littleSideMarge'>
+
+        <p class='colorWhite'>IMAGE</p>
+
+        </div>
+        
+        <div class='containerSpaceBetween '>
+            <p class='colorWhite textSmall'>Jean Michel</p>
+            <div class='containerMiddle'>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+
+            </div>
+        </div>
+        
+
+    </div>
+
+    <div class ='card backgroundDarkBlue'>
+
+        <div class='containerSpaceBetween '>
+            <h1 class='colorWhite'>J'adore beaucoup les saucisses, mais genre vraiment</h1>
+            <p class='colorWhite'>Restaurant</p>
+            
+
+        </div>
+
+        <div class='containerRightColumns littleSideMarge'>
+
+        <p class='colorWhite'>IMAGE</p>
+
+        </div>
+        
+        <div class='containerSpaceBetween '>
+            <p class='colorWhite textSmall'>Jean Michel</p>
+            <div class='containerMiddle'>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+
+            </div>
+        </div>
+        
+
+    </div>
+
+    <div class ='card backgroundDarkBlue'>
+
+        <div class='containerSpaceBetween '>
+            <h1 class='colorWhite'>J'adore beaucoup les saucisses, mais genre vraiment</h1>
+            <p class='colorWhite'>Restaurant</p>
+            
+
+        </div>
+
+        <div class='containerRightColumns littleSideMarge'>
+
+        <p class='colorWhite'>IMAGE</p>
+
+        </div>
+        
+        <div class='containerSpaceBetween '>
+            <p class='colorWhite textSmall'>Jean Michel</p>
+            <div class='containerMiddle'>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+
+            </div>
+        </div>
+        
+
+    </div>
+
+    <div class ='card backgroundDarkBlue'>
+
+        <div class='containerSpaceBetween '>
+            <h1 class='colorWhite'>J'adore beaucoup les saucisses, mais genre vraiment</h1>
+            <p class='colorWhite'>Restaurant</p>
+            
+
+        </div>
+
+        <div class='containerRightColumns littleSideMarge'>
+
+        <p class='colorWhite'>IMAGE</p>
+
+        </div>
+        
+        <div class='containerSpaceBetween '>
+            <p class='colorWhite textSmall'>Jean Michel</p>
+            <div class='containerMiddle'>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+                <p class = 'noMarge'><span class='star'>☆</span></p>
+
+            </div>
+        </div>
+        
+
+    </div>
 
 </div>
 
