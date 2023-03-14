c08a9309d12655423c2514479268b5414e5a8b59
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-03-14 15:03:38 +0100

    commentaires controllers

--- website/controller/ControllerMain.php
+++ website/controller/ControllerMain.php
@@ -1,17 +1,27 @@
 <?php 
+/**
+ * Auteur : João Ferreira & Samuel Fernandez
+ * Date : 10.03.2023
+ * Description : Contrôleur de la page d'accueil
+ */
 
 include_once "Controller.php";
 include_once "model/ModelCif.php";
 include_once "model/ModelUser.php";
 include_once "view/view.php";
 
+// hérite de la classe Controller
 class ControllerMain extends Controller{
     private $cif;
 
+     /**
+    * Constructeur de la classe ControllerMain, donne des variables de model à la view, génére la page spécifique
+    */
     public function __construct() {
         $this->cif = new ModelCif();
         $this->view = new View();
 
+        // récupération des 5 derniers CIFs
         $array = $this->cif->getLimitCif(5);
         $this->view->render('main.php', $array);
     }
